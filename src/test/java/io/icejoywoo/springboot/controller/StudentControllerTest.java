5bfa8f1cf95fade5829d0dbade9faf060eacc078
icejoywoo <icejoywoo@gmail.com>
2024-10-30 22:29:39 +0800

    add unit test

--- src/test/java/io/icejoywoo/springboot/controller/StudentControllerTest.java
+++ src/test/java/io/icejoywoo/springboot/controller/StudentControllerTest.java
@@ -13,8 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Optional;
-
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -48,5 +46,66 @@ public class StudentControllerTest {
                 .andExpect(jsonPath("$.data.email").value(student.getEmail()));
 
         verify(studentService, times(1)).getStudentById(id);
+    }
+
+    @Test
+    public void testAddNewStudent() throws Exception {
+        long id = 1;
+        Student student = new Student();
+        student.setId((int) id);
+        student.setName("test");
+        student.setEmail("test@test.com");
+        student.setAge(20);
+
+        StudentDTO studentDTO = StudentConverter.convertStudentToStudentDTO(student);
+        when(studentService.addNewStudent(studentDTO)).thenReturn(id);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/student")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"id\":1, \"name\":\"test\",\"email\":\"test@test.com\"}")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.data").value(id));
+
+        verify(studentService, times(1)).addNewStudent(studentDTO);
+    }
+
+    @Test
+    public void testDeleteStudentById() throws Exception {
+        long id = 1;
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.delete(String.format("/student/%d", id))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk());
+
+        verify(studentService, times(1)).deleteStudentById(id);
+    }
+
+    @Test
+    public void testUpdateStudentById() throws Exception {
+        long id = 1;
+
+        StudentDTO ret = new StudentDTO();
+        ret.setId(id);
+        ret.setName("foo");
+        ret.setEmail("foo@123.com");
+        when(studentService.updateStudentById(id, "foo", "foo@123.com")).thenReturn(ret);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.put(String.format("/student/%d", id))
+                                .formField("name", "foo")
+                                .formField("email", "foo@123.com")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.data.id").value(id))
+                .andExpect(jsonPath("$.data.name").value("foo"))
+                .andExpect(jsonPath("$.data.email").value("foo@123.com"));
+
+        verify(studentService, times(1)).updateStudentById(id, "foo", "foo@123.com");
     }
 }
